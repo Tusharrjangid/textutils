10ca80ed4b5d225b0112150b28f8d1667e5d57fa
Tusharrjangid <71558301+Tusharrjangid@users.noreply.github.com>
2022-08-27 11:55:33 +0530

    Updates

---
+++ precache-manifest.cfe96def5b2cf9762d17ed2668ba1a35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cedbf19ab95da00c491",
+    "url": "/textutils/static/css/main.43a55615.chunk.css"
+  },
+  {
+    "revision": "5cedbf19ab95da00c491",
+    "url": "/textutils/static/js/main.5cedbf19.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/textutils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/textutils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "8c5d3a7ccc996c2dc493",
+    "url": "/textutils/static/js/runtime~main.8c5d3a7c.js"
+  },
+  {
+    "revision": "a9dbd96207a046a3418ded0bd3ed0af4",
+    "url": "/textutils/index.html"
+  }
+];
